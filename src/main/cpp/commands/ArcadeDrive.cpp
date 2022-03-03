c237e388c908e8a0882e902d08848fe59a624135
Anthony Chen <chenanthony365@gmail.com>
2022-03-03 09:42:08 -0500

    create arcadedrive command based on example

---
+++ src/main/cpp/commands/ArcadeDrive.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/ArcadeDrive.h"
+
+#include "Robot.h"
+
+// Initialises the command with its name and requirements
+ArcadeDrive::ArcadeDrive(std::function<double()> speed, std::function<double()> rotation, Drivetrain& drivetrain)
+    : m_speed(std::move(speed)), m_rotation(std::move(rotation)), m_drivetrain(&drivetrain) {
+    SetName("ArcadeDrive");
+    AddRequirements({m_drivetrain});
+}
+
+// Called repeatedly when this Command is scheduled to run
+void ArcadeDrive::Execute() {
+    m_drivetrain->Drive(m_speed(), m_rotation());
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool TankDrive::IsFinished() {
+  return false;
+}
+
+// Called once after isFinished returns true
+void TankDrive::End(bool) {
+  m_drivetrain->Drive(0, 0);
+}
